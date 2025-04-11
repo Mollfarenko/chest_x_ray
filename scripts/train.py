049f9273c1003084fae4442d09aa89e4987cb4dc
Vitaliy <rogovykvitaliy@gmail.com>
2025-04-11 07:04:26 +0200

    Created first simple CNN model

--- scripts/train.py
+++ scripts/train.py
@@ -1,10 +1,20 @@
+import sys
+import os
+
+# Add the root directory of the project to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from utils.dataloader import get_dataloaders
 
 # Path to your dataset folder
-data_dir = "../data"
+data_dir = "data"
 
+# Load dataloaders
 dataloaders = get_dataloaders(data_dir)
 
-train_loader = dataloaders["train"]
-val_loader = dataloaders["val"]
-test_loader = dataloaders["test"]
+# Count the number of samples in each loader
+for split in ["train", "val", "test"]:
+    loader = dataloaders[split]
+    dataset_size = len(loader.dataset)
+    print(f"{split.capitalize()} set: {dataset_size} images")
+
